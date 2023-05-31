087809e61e1df0a70e18fcc836b499fcccf51f60
brandraw <ckddnri22@gmail.com>
2023-06-01 01:58:53 +0900

    add test page

---
+++ resources/views/test-page01.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="py-5 hero container-fluid">
+        <h1>This is test page</h1>
+    </div>
+@endsection
+
+@push('scripts')
+
+@endpush
